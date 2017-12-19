9ec25e7810404bf4f55b6f995d83434fede077e1
AKondrashkin <anton.kondrashkin@gmail.com>
2017-12-19 09:03:43 +0300

    Дима не смог

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 
 $('#enemy > table td').on('click', function()
 {
-	var shot_id = $('#enemy > table td').attr('id');
+	var shot_id = $(this).attr('id');
 	sendShot = new XMLHttpRequest();
 	sendShot.open('POST', '../sendShotAI', true);
 	sendShot.send('login=!' + getCook("login") + "!shot=!" + shot_id + "!");
@@ -10,9 +10,61 @@ $('#enemy > table td').on('click', function()
 			if (sendShot.readyState == 4) {
 				if (sendShot.responseText == "1")
 				{
-					
+					setShipsAfterShot();
 					 
 				}
 			}
 			};
 });
+
+
+function setShipsAfterShot()
+{
+	getShips=new XMLHttpRequest();
+	var nameJson = "new_" + getCook('login') + ".json";
+    getShips.open('GET','../json/' + nameJson,true);
+    getShips.send();
+	getShips.onreadystatechange=function() 
+		{
+			if (getShips.readyState==4)
+				{ 
+				    var jsonShip=eval( '('+getShips.responseText+')' );
+					var jsonMatrPlayer = jsonShip.game[0].ships;
+					for (var i = 0; i < 10; i++)
+					{
+						for (var j = 0; j < 10; j++)
+						{
+							var deck = getDeck(i + '_' + j,jsonMatrPlayer);
+							if (deck == '1' || deck == '2' || deck =='3' || deck == '4')
+							{
+								$('#' + i + '_' + j).addClass('hereShip')
+							}
+							if (deck == '-1' || deck == '-2' || deck =='-3' || deck == '-4' || deck == '0')
+							{
+								$('#' + i + '_' + j).addClass('hitting')
+							}
+							if (deck == '-5')
+							{
+								$('#' + i + '_' + j).addClass('missing')
+							}
+						}
+					}
+					jsonMatrPlayer = jsonShip.game[1].ships;
+					for (var i = 0; i < 10; i++)
+					{
+						for (var j = 0; j < 10; j++)
+						{
+							var deck = getDeck(i + '_' + j,jsonMatrPlayer);
+							if (deck == '-1' || deck == '-2' || deck =='-3' || deck == '-4' || deck == '0')
+							{
+								$('#' + i + '_' + j + '_E').addClass('hitting')
+							}
+							if (deck == '-5')
+							{
+								$('#' + i + '_' + j + '_E').addClass('missing')
+							}
+						}
+					}
+					}
+				}
+		}
